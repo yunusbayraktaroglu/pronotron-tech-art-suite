9b2452c45ad6fc55d4de7ff0cd2845e678b7a6ea
Yunus Bayraktaroğlu <yunus.bayraktaroglu@gmail.com>
2025-10-20 05:56:22 +0300

    fix(eslint): upgrade version and switch configuration files

--- packages/config-eslint/library.js
+++ packages/config-eslint/library.js
@@ -1,67 +1,41 @@
-const { resolve } = require( "node:path" );
+import stylistic from '@stylistic/eslint-plugin';
 
-const project = resolve( process.cwd(), "tsconfig.json" );
+import { config as baseConfig } from "./base.js";
 
-/** @type {import("eslint").Linter.Config} */
-module.exports = {
-	extends: [ 
-		"eslint:recommended",
-		"plugin:@typescript-eslint/eslint-recommended",
-		"plugin:@typescript-eslint/recommended", 
-		"prettier", 
-		"turbo",
-	],
-	plugins: [ 
-		"@typescript-eslint", 
-		"only-warn" 
-	],
-	globals: {
-		React: true,
-		JSX: true,
-	},
-	env: {
-		node: true,
-	},
-	settings: {
-		"import/resolver": {
-			typescript: {
-				project,
-			},
+/**
+ * A shared TS library configuration for the packages.
+ *
+ * @type {import("eslint").Linter.Config[]}
+ * */
+export const config = [
+	...baseConfig,
+	{
+		plugins: {
+			'@stylistic': stylistic
 		},
-	},
-	ignorePatterns: [
-		// Ignore dotfiles
-		".*.js",
-		"node_modules/",
-		"dist/",
-	],
-	overrides: [
-		{
-			files: ["*.js?(x)", "*.ts?(x)"],
+		rules: {
+			"@typescript-eslint/no-use-before-define": "off",
+			"@typescript-eslint/no-unused-vars": [ "warn", {
+				"argsIgnorePattern": "^_"
+			}],
+			"@typescript-eslint/no-var-requires": [ "off" ],
+			"@typescript-eslint/no-non-null-assertion": [ "off" ],
+			"@stylistic/type-annotation-spacing": [ "error", { "after": true } ],
+			"max-len": [ "warn", {
+				"code": 120,
+				"tabWidth": 4,
+				"ignoreUrls": true,
+				"ignorePattern": "^import|^export"
+			} ],
+			"semi": [ "error" ],
+			"eqeqeq": [ "warn", "smart" ],
+			"space-infix-ops": [ "warn" ],
+			"space-in-parens": [ "error", "always" ],
+			"object-curly-spacing": [ "error", "always" ],
+			"array-bracket-spacing": [ "error", "always" ],
+			"key-spacing": [ "error", { "afterColon": true } ],
+			"no-irregular-whitespace": [ "warn" ],
+			"comma-spacing": [ "warn" ]
 		},
-	],
-	rules: {
-		"@typescript-eslint/no-use-before-define": "off",
-		"@typescript-eslint/no-unused-vars": [ "warn", {
-			"argsIgnorePattern": "^_"
-		}],
-		"@typescript-eslint/no-var-requires": [ "off" ],
-		"@typescript-eslint/no-non-null-assertion": [ "off" ],
-		"@typescript-eslint/type-annotation-spacing": [ "error", { "after": true } ],
-		"max-len": [ "warn", {
-			"code": 120,
-			"tabWidth": 4,
-			"ignoreUrls": true,
-			"ignorePattern": "^import|^export"
-		} ],
-		"semi": [ "error" ],
-		"eqeqeq": [ "warn", "smart" ],
-		"space-infix-ops": [ "warn" ],
-		"space-in-parens": [ "error", "always" ],
-		"object-curly-spacing": [ "error", "always" ],
-		"array-bracket-spacing": [ "error", "always" ],
-		"key-spacing": [ "error", { "afterColon": true } ],
-		"no-irregular-whitespace": [ "warn" ],
-		"comma-spacing": [ "warn" ]
-	},
-};
+	}
+];
