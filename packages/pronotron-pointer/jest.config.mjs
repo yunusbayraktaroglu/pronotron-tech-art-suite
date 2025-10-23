a369c303f0eff014dd7132d2819ac5cd988c8da0
Yunus Bayraktaroğlu <yunus.bayraktaroglu@gmail.com>
2025-10-24 02:47:25 +0300

    test(io,pointer,utils): add jest configuration

---
+++ packages/pronotron-pointer/jest.config.mjs
@@ -0,0 +1,11 @@
+import { createDefaultPreset } from "ts-jest";
+
+const defaultPreset = createDefaultPreset();
+
+export default {
+	...defaultPreset,
+	testMatch: [ "**/tests/*.test.ts" ],
+	testEnvironment: "jsdom",
+	collectCoverage: true,
+	coverageReporters: [ "text", "html" ],
+};
